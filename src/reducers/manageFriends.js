2398b4eb60ee28a7602e409d8ce795649d04d4bf
no0ro <nrolwes@gmail.com>
2020-12-09 08:09:50 -0500

    commit tree c7a76cc906b07415aeca25584641d03313dfbb83

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,33 @@
-export function manageFriends(state, action){
+export function manageFriends(state = {friends: []}, action){
+    switch (action.type) {
+
+        case "ADD_FRIEND":
+            return ({...state, friends: [...state.friends, action.friend]})
+                
+        case "REMOVE_FRIEND":
+            // find the state id: key's value, that matches the action.id's value. 
+            // save the full state obj that contains the id to a variable 
+            let removeThisIndex = state.friends.findIndex( friend => friend.id === action.id)
+            return ({...state, friends: [...state.friends.slice(0, removeThisIndex), ...state.friends.slice(removeThisIndex + 1)]})
+            // add into state-- whats BEFORE and whats AFTER the obj we wanted removed
+            // aka never really REMOVE the obj...just dont include/invite it to the next state
+        default: 
+            return state;
+    }
 }
+
+
+
+// When adding a friend, the action will include a friend key 
+// assigned to an object with name, hometown, and id keys.
+
+//     ```js
+//       action = {
+//         type: "ADD_FRIEND",
+//         friend: {
+//           name: "Chrome Boi"
+//           homewtown: "NYC",
+//           id: 1
+//         }
+//       }
+//     ```
